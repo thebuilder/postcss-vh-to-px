df7056b54f5e261cf79373cd47372eb522fcb650
Daniel Schmidt <dsc@insilico.dk>
2017-03-13 17:15:00 +0100

    Fix eslint

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const postcss = require('postcss');
 const defaults = {
-  unitSize: 6.67
+    unitSize: 6.67
 };
 
 module.exports = postcss.plugin('PLUGIN_NAME', (opts) => {
-    const options = Object.assign({}, defaults, opts)
+    const options = Object.assign({}, defaults, opts);
 
     return (root) => {
-        root.replaceValues(/\d+vh/g, {fast: 'vh'}, val => {
+        root.replaceValues(/\d+vh/g, { fast: 'vh' }, val => {
             return options.unitSize * parseInt(val) + 'px';
         });
     };
